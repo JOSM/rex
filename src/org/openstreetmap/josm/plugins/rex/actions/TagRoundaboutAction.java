c6f7e6fc6a41ed9b514878e39fad3b624dd2386f
Gorm <josm@gorm.cc>
2015-02-16 00:31:28 +0100

    Now makes 'circle'

--- src/org/openstreetmap/josm/plugins/rex/actions/TagRoundaboutAction.java
+++ src/org/openstreetmap/josm/plugins/rex/actions/TagRoundaboutAction.java
@@ -1,20 +1,21 @@
 package org.openstreetmap.josm.plugins.rex.actions;
 
 import javax.swing.JOptionPane;
+
+import java.util.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.command.*;
-import java.util.*;
-import java.util.LinkedList;
-import java.awt.event.KeyEvent;
 import org.openstreetmap.josm.tools.Shortcut;
-import java.awt.event.ActionEvent;
 import org.openstreetmap.josm.actions.JosmAction;
-import org.openstreetmap.josm.command.MoveCommand;
 import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.data.osm.Node;
 import org.openstreetmap.josm.data.osm.Way;
-import org.openstreetmap.josm.data.osm.OsmPrimitive;
 import org.openstreetmap.josm.gui.Notification;
+
 import static org.openstreetmap.josm.tools.I18n.tr;
 
 /**
@@ -26,12 +27,10 @@ public class TagRoundaboutAction extends JosmAction {
     private static final String TITLE = tr("Add Roundabout Tag");
 
     public TagRoundaboutAction() {
-
         super(tr("REX"), "images/dialogs/logo-rex.png",
-            tr("Roundabout Expander"),
-            Shortcut.registerShortcut("menu:rex", tr("Menu: {0}", tr("Roundabout Expander")),
-                KeyEvent.VK_R, Shortcut.ALT_CTRL), false);
-
+                tr("Roundabout Expander"),
+                Shortcut.registerShortcut("menu:rex", tr("Menu: {0}", tr("Roundabout Expander")),
+                    KeyEvent.VK_R, Shortcut.ALT_CTRL), false);
     }
 
     @Override
@@ -46,25 +45,14 @@ public class TagRoundaboutAction extends JosmAction {
                 //TODO a possible intermediate step before creating a real circle:
                 //Set some tags
                 Main.main.undoRedo.add(new ChangePropertyCommand(selection, "highway", "mini_roundabout"));
-
-                //Move a bit
-                Node n = (Node) selectedNodes.toArray()[0];
-                LatLon coor2 = moveHeadingDistance(n.getCoor(), 1, 6);
-                Main.main.undoRedo.add(new MoveCommand(n, coor2));
-                Main.map.mapView.repaint();
-
-               return;
+                return;
             }
 
-            //We have 3 nodes selected, make a closed way with them
-            if(  selectedNodes.size() >= 3) {
-                new Notification(
-                        tr("Making a way of the nodes"))
-                    .setIcon(JOptionPane.WARNING_MESSAGE)
-                    .show();
-                
+            //We have enough nodes selected, make a closed way with them
+            if(  selectedNodes.size() >= 2) {
+
                 for (Node n : selectedNodes) {
-                     moveWayEndNodeTowardsNextNode(n);
+                    moveWayEndNodeTowardsNextNode(n, -6);
                 }
                 makeCircle(selectedNodes);
 
@@ -72,20 +60,21 @@ public class TagRoundaboutAction extends JosmAction {
             }
 
             //We don't have a suitable selection
-            new Notification(
-                    tr("No appropriate selection. Try selecting a single node first."))
-                .setIcon(JOptionPane.WARNING_MESSAGE)
-                .show();
+            pri( tr("No appropriate selection. Try selecting a single node first."));
         }
 
     /**
      * Create a roundabout way
+     *
+     * @param List nordes   Nodes to assemble into circle
+     * @param Node original Original, central node. Tagging determines direction.
      */
     public void makeCircle(List<Node> nodes) {
         //Create the way
         Way circle = new Way();
 
-        //sort nodes list on heading from original expanded node and counter_clockwise
+        //sort nodes list on heading from original expanded node
+        //takes into account direction of original mini roundabout
         //TODO
 
         //add the nodes to the way
@@ -106,56 +95,42 @@ public class TagRoundaboutAction extends JosmAction {
     }
 
     /**
-    * A selected node, being the first or last node in a way, move it x meter towards the next node
-    *
-    * @param Node   endnode  Node to be moved
-    * @param double distance Distance to move node
-    */
-    public void moveWayEndNodeTowardsNextNode( Node endnode) {
+     * A selected node, being the first or last node in a way, move it x meter towards the next node
+     *
+     * @param Node   node  Node to be moved
+     * @param double distance Distance to move node in meter
+     */
+    public void moveWayEndNodeTowardsNextNode( Node node, double distance) {
         //some verification:
-        List<Way> referedWays = OsmPrimitive.getFilteredList(endnode.getReferrers(), Way.class);
+        List<Way> referedWays = OsmPrimitive.getFilteredList(node.getReferrers(), Way.class);
+
         //node is member of one way
-        if (referedWays.size() == 1) {
-    
-            new Notification(
-                    tr("Yay! The node is in just one way!"))
-                .setIcon(JOptionPane.WARNING_MESSAGE)
-                .show();
-        } else {
+        if (referedWays.size() != 1) {
             return;
         }
 
+        //Node is only member of one way
         Way way = referedWays.get(0);
-        if (way.isFirstLastNode(endnode)) {
-    
-            new Notification(
-                    tr("Yay! The node is a end node!"))
-                .setIcon(JOptionPane.WARNING_MESSAGE)
-                .show();
-        } else {
+        if (!way.isFirstLastNode(node)) {
             return;
         }
 
-        if(way.getNodes().size() >= 2){
-    
-            new Notification(
-                    tr("Yay! The way has min two nodes!"))
-                .setIcon(JOptionPane.WARNING_MESSAGE)
-                .show();
-        } else {
+        //Way must be at least two nodes long
+        if(way.getNodesCount() < 2){
             return;
         }
 
-        /*
-           Node nextnode = 1;
-           if (endnode.position_in_way(way) != 0) {
-           nextnode = way.number_of_nodes()-1;
-           }
-           double heading = endnode.heading(nextnode.getCoor());
-           double distance = 6;
-           LatLon newpos = moveHeadingDistance(node, heading, distance);
-           endnode.setLatLon(newpos);
-           */
+        //Find heading to next node
+        Node ajacent_node = way.getNeighbours(node).iterator().next();
+        double heading = node.getCoor().heading(ajacent_node.getCoor());
+
+        //Move the node towards the next node
+        LatLon newpos = moveHeadingDistance(node.getCoor(), heading, distance);
+        node.setCoor(newpos);
+    }
+
+    public void pri(String str) {
+        new Notification( str) .setIcon(JOptionPane.WARNING_MESSAGE) .show();
     }
 
     /**
@@ -167,36 +142,23 @@ public class TagRoundaboutAction extends JosmAction {
      */
     public LatLon moveHeadingDistance(LatLon start, double heading, double distance)
     {
-        double lat2 = start.lat() + 0.00001 * distance;
-        double lon2 = start.lon() + 0.00001 * distance; 
+        double  R = 6378100; //Radius of the Earth in meters
+
+        double lat1 = Math.toRadians(start.lat());
+        double lon1 = Math.toRadians(start.lon());
+
+        double lat2 = Math.asin( Math.sin(lat1) * Math.cos(distance/R) +
+                Math.cos(lat1) * Math.sin(distance/R) * Math.cos(heading));
+
+        double lon2 = lon1 + Math.atan2(Math.sin(heading) * Math.sin(distance/R) * Math.cos(lat1),
+                Math.cos(distance/R) - Math.sin(lat1) * Math.sin(lat2));
+
+        lat2 = Math.toDegrees(lat2);
+        lon2 = Math.toDegrees(lon2);
+
         return new LatLon(lat2, lon2);
- /*
- * import math
- *
- * R = 6378.1 #Radius of the Earth
- * brng = 1.57 #Bearing is 90 degrees converted to radians.
- * d = 15 #Distance in km
- *
- * #lat2  52.20444 - the lat result I'm hoping for
- * #lon2  0.36056 - the long result I'm hoping for.
- *
- * lat1 = math.radians(52.20472) #Current lat point converted to radians
- * lon1 = math.radians(0.14056) #Current long point converted to radians
- *
- * lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
- *      math.cos(lat1)*math.sin(d/R)*math.cos(brng))
- *
- *      lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
- *                   math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
- *
- *                   lat2 = math.degrees(lat2)
- *                   lon2 = math.degrees(lon2)
- *
- *                   print(lat2)
- *                   print(lon2)
-    */
-}
- 
+    }
+
 
     @Override
         protected void updateEnabledState() {
@@ -215,3 +177,5 @@ public class TagRoundaboutAction extends JosmAction {
 
         }
 }
+
+//EOF
